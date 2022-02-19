6af16f5f369e2293e091a4c84ddd6112767446d4
Beck Bjella <beckbjella@gmail.com>
2022-02-18 19:25:29 -0600

    update

--- main.py
+++ main.py
@@ -3,27 +3,33 @@ import numpy as np
 import cv2 as cv
 from grip import GripPipeline
 
-capture = cv.VideoCapture(0)
 
-if not capture.isOpened():
-    raise IOError("Cannot open webcam")
-
-pipeline = GripPipeline()
-
-while True:
-    ret, frame = capture.read()
-
-    pipeline.process(frame)
-    x, y, w, h = cv.boundingRect(numpy.array(pipeline.cv_erode_output))
-
+def render_frame(ball_x, ball_y):
     canvas = np.zeros((480, 640, 3), dtype="uint8")
 
-    canvas = cv.circle(canvas, (x, y), int(w / 2), (0, 0, 255), -1)
+    canvas = cv.circle(canvas, (ball_x, ball_y), int(w / 2), (0, 0, 255), -1)
 
     cv.imshow("Canvas", canvas)
-    c = cv.waitKey(1)
-    if c == 27:
-        break
 
-capture.release()
-cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    capture = cv.VideoCapture(0)
+    if not capture.isOpened():
+        raise IOError("Cannot open webcam")
+
+    pipeline = GripPipeline()
+
+    while True:
+        ret, frame = capture.read()
+
+        pipeline.process(frame)
+        x, y, w, h = cv.boundingRect(numpy.array(pipeline.cv_erode_output))
+
+        render_frame(x, y)
+
+        c = cv.waitKey(1)
+        if c == 27:
+            break
+
+    capture.release()
+    cv.destroyAllWindows()
